3208753e9df03d083a6fa4bf4ed41a1fb91043b3
Samuel Dervishi <samueldervishi02@gmail.com>
2023-06-29 20:42:11 +0200

    updates

--- script.js
+++ script.js
@@ -1,122 +1,108 @@
-var blockSize = 25;
-var total_row = 17; //total row number
-var total_col = 17; //total column number
-var board;
-var context;
+// Number to decide the game digit i.e.
+// game work on 2 digit, 3 digit or n
+// digit of number.
+const digit = 100;
 
-var snakeX = blockSize * 5;
-var snakeY = blockSize * 5;
+// Set random numbers. The task of user
+// is to find these numbers.
+let num1 = Math.floor(Math.random() * digit);
+let num2 = Math.floor(Math.random() * digit);
+let num3 = Math.floor(Math.random() * digit);
 
-// Set the total number of rows and columns
-var speedX = 0; //speed of snake in x coordinate.
-var speedY = 0; //speed of snake in Y coordinate.
+// Hints are generated here onwards.
+// Hint 1
+let h1_a = Math.floor(Math.random() * digit);
+let h1_b = Math.floor(Math.random() * digit);
+let h1_c = num3;
 
-var snakeBody = [];
+// Hint 2
+let h2_a = Math.floor(Math.random() * digit);
+let h2_b = Math.floor(Math.random() * digit);
+let h2_c = num2;
 
-var foodX;
-var foodY;
+//Hint 3
+let h3_a = num2;
+let h3_b = num1;
+let h3_c = Math.floor(Math.random() * digit);
 
-var gameOver = false;
+// Hint 4
+let h4_a = Math.floor(Math.random() * digit);
+let h4_b = Math.floor(Math.random() * digit);
+let h4_c = Math.floor(Math.random() * digit);
 
-window.onload = function () {
-	// Set board height and width
-	board = document.getElementById("board");
-	board.height = total_row * blockSize;
-	board.width = total_col * blockSize;
-	context = board.getContext("2d");
+// Hint 5
+let h5_a = Math.floor(Math.random() * digit);
+let h5_b = Math.floor(Math.random() * digit);
+let h5_c = num1;
 
-	placeFood();
-	document.addEventListener("keyup", changeDirection); //for movements
-	// Set snake speed
-	setInterval(update, 1000 / 10);
-}
+// Hint generation ends
 
-function update() {
-	if (gameOver) {
-		return;
-	}
+// Putting hints to index.html page
+document.getElementById('h1').innerHTML =
+	`<input type="text" id="b1" value="${h1_a}
+	" readonly> <input type="text" id="b1" value="
+	${h1_b}" readonly> <input type="text" id="b1"
+	value="${h1_c}" readonly> `;
 
-	// Background of a Game
-	context.fillStyle = "green";
-	context.fillRect(0, 0, board.width, board.height);
+document.getElementById('h2').innerHTML =
+	`<input type="text" id="b1" value="${h2_a}
+	" readonly> <input type="text" id="b1"
+	value="${h2_b}" readonly> <input type="text"
+	id="b1" value="${h2_c}" readonly> `;
 
-	// Set food color and position
-	context.fillStyle = "yellow";
-	context.fillRect(foodX, foodY, blockSize, blockSize);
+document.getElementById('h3').innerHTML =
+	`<input type="text" id="b1" value="${h3_a}
+	" readonly> <input type="text" id="b1"
+	value="${h3_b}" readonly> <input type="text"
+	id="b1" value="${h3_c}" readonly> `;
 
-	if (snakeX == foodX && snakeY == foodY) {
-		snakeBody.push([foodX, foodY]);
-		placeFood();
-	}
+document.getElementById('h4').innerHTML =
+	`<input type="text" id="b1" value="${h4_a}
+	" readonly> <input type="text" id="b1"
+	value="${h4_b}" readonly> <input type="text"
+	id="b1" value="${h4_c}" readonly> `;
 
-	// body of snake will grow
-	for (let i = snakeBody.length - 1; i > 0; i--) {
-		// it will store previous part of snake to the current part
-		snakeBody[i] = snakeBody[i - 1];
-	}
-	if (snakeBody.length) {
-		snakeBody[0] = [snakeX, snakeY];
-	}
+document.getElementById('h5').innerHTML =
+	`<input type="text" id="b1" value="${h5_a}
+	" readonly> <input type="text" id="b1"
+	value="${h5_b}" readonly> <input type="text"
+	id="b1" value="${h5_c}" readonly> `;
 
-	context.fillStyle = "white";
-	snakeX += speedX * blockSize; //updating Snake position in X coordinate.
-	snakeY += speedY * blockSize; //updating Snake position in Y coordinate.
-	context.fillRect(snakeX, snakeY, blockSize, blockSize);
-	for (let i = 0; i < snakeBody.length; i++) {
-		context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
-	}
+// Function to check whether game is solved or not
+function myfunc() {
 
-	if (snakeX < 0
-		|| snakeX > total_col * blockSize
-		|| snakeY < 0
-		|| snakeY > total_row * blockSize) {
-		
-		// Out of bound condition
-		gameOver = true;
-		alert("Game Over");
-	}
+	// Getting value of user though input fields.
+	let a = document.getElementById('b1').value;
+	let b = document.getElementById('b2').value;
+	let c = document.getElementById('b3').value;
 
-	for (let i = 0; i < snakeBody.length; i++) {
-		if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
-			
-			// Snake eats own body
-			gameOver = true;
-			alert("Game Over");
+	// Checking whether input fields is blank or not
+	if (a != '' && b != '' && c != '') {
+		if (a == num1 && b == num2 && c == num3) {
+
+			// Outputting this message to index.html
+			// page with id result.
+			$('#result').html('You Crack it.????');
+
+			// Opening popup modal
+			$('#popup').modal('toggle');
+		} else {
+
+			// Outputting this message to index.html
+			// page with id result.
+			$('#result').html('Try once again.????');
+
+			// Opening popup modal
+			$('#popup').modal('toggle')
 		}
 	}
-}
+	else {
+		
+		// Outputting this message to index.html
+		// page with id result.
+		$('#result').html('Fill all fields.????');
 
-// Movement of the Snake - We are using addEventListener
-function changeDirection(e) {
-	if (e.code == "ArrowUp" && speedY != 1) {
-		// If up arrow key pressed with this condition...
-		// snake will not move in the opposite direction
-		speedX = 0;
-		speedY = -1;
+		// Opening popup modal
+		$('#popup').modal('toggle');
 	}
-	else if (e.code == "ArrowDown" && speedY != -1) {
-		//If down arrow key pressed
-		speedX = 0;
-		speedY = 1;
-	}
-	else if (e.code == "ArrowLeft" && speedX != 1) {
-		//If left arrow key pressed
-		speedX = -1;
-		speedY = 0;
-	}
-	else if (e.code == "ArrowRight" && speedX != -1) {
-		//If Right arrow key pressed
-		speedX = 1;
-		speedY = 0;
-	}
-}
-
-// Randomly place food
-function placeFood() {
-
-	// in x coordinates.
-	foodX = Math.floor(Math.random() * total_col) * blockSize;
-	
-	//in y coordinates.
-	foodY = Math.floor(Math.random() * total_row) * blockSize;
 }
