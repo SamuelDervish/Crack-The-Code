1c108806c25ae749ada14eef4b83fecc5925550f
Samuel Dervishi <samueldervishi02@gmail.com>
2023-06-29 20:36:09 +0200

    updates

--- script.js
+++ script.js
@@ -1,117 +1,122 @@
+var blockSize = 25;
+var total_row = 17; //total row number
+var total_col = 17; //total column number
+var board;
+var context;
 
-const selectBox = document.querySelector(".select-box"),
-selectBtnX = selectBox.querySelector(".options .playerX"),
-selectBtnO = selectBox.querySelector(".options .playerO"),
-playBoard = document.querySelector(".play-board"),
-players = document.querySelector(".players"),
-allBox = document.querySelectorAll("section span"),
-resultBox = document.querySelector(".result-box"),
-wonText = resultBox.querySelector(".won-text"),
-replayBtn = resultBox.querySelector("button");
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
 
-window.onload = ()=>{
-    for (let i = 0; i < allBox.length; i++) {
-       allBox[i].setAttribute("onclick", "clickedBox(this)");
-    }
+// Set the total number of rows and columns
+var speedX = 0; //speed of snake in x coordinate.
+var speedY = 0; //speed of snake in Y coordinate.
+
+var snakeBody = [];
+
+var foodX;
+var foodY;
+
+var gameOver = false;
+
+window.onload = function () {
+	// Set board height and width
+	board = document.getElementById("board");
+	board.height = total_row * blockSize;
+	board.width = total_col * blockSize;
+	context = board.getContext("2d");
+
+	placeFood();
+	document.addEventListener("keyup", changeDirection); //for movements
+	// Set snake speed
+	setInterval(update, 1000 / 10);
 }
 
-selectBtnX.onclick = ()=>{
-    selectBox.classList.add("hide");
-    playBoard.classList.add("show");
+function update() {
+	if (gameOver) {
+		return;
+	}
+
+	// Background of a Game
+	context.fillStyle = "green";
+	context.fillRect(0, 0, board.width, board.height);
+
+	// Set food color and position
+	context.fillStyle = "yellow";
+	context.fillRect(foodX, foodY, blockSize, blockSize);
+
+	if (snakeX == foodX && snakeY == foodY) {
+		snakeBody.push([foodX, foodY]);
+		placeFood();
+	}
+
+	// body of snake will grow
+	for (let i = snakeBody.length - 1; i > 0; i--) {
+		// it will store previous part of snake to the current part
+		snakeBody[i] = snakeBody[i - 1];
+	}
+	if (snakeBody.length) {
+		snakeBody[0] = [snakeX, snakeY];
+	}
+
+	context.fillStyle = "white";
+	snakeX += speedX * blockSize; //updating Snake position in X coordinate.
+	snakeY += speedY * blockSize; //updating Snake position in Y coordinate.
+	context.fillRect(snakeX, snakeY, blockSize, blockSize);
+	for (let i = 0; i < snakeBody.length; i++) {
+		context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+	}
+
+	if (snakeX < 0
+		|| snakeX > total_col * blockSize
+		|| snakeY < 0
+		|| snakeY > total_row * blockSize) {
+		
+		// Out of bound condition
+		gameOver = true;
+		alert("Game Over");
+	}
+
+	for (let i = 0; i < snakeBody.length; i++) {
+		if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+			
+			// Snake eats own body
+			gameOver = true;
+			alert("Game Over");
+		}
+	}
 }
 
-selectBtnO.onclick = ()=>{ 
-    selectBox.classList.add("hide");
-    playBoard.classList.add("show");
-    players.setAttribute("class", "players active player");
+// Movement of the Snake - We are using addEventListener
+function changeDirection(e) {
+	if (e.code == "ArrowUp" && speedY != 1) {
+		// If up arrow key pressed with this condition...
+		// snake will not move in the opposite direction
+		speedX = 0;
+		speedY = -1;
+	}
+	else if (e.code == "ArrowDown" && speedY != -1) {
+		//If down arrow key pressed
+		speedX = 0;
+		speedY = 1;
+	}
+	else if (e.code == "ArrowLeft" && speedX != 1) {
+		//If left arrow key pressed
+		speedX = -1;
+		speedY = 0;
+	}
+	else if (e.code == "ArrowRight" && speedX != -1) {
+		//If Right arrow key pressed
+		speedX = 1;
+		speedY = 0;
+	}
 }
 
-let playerXIcon = "fas fa-times";
-let playerOIcon = "far fa-circle";
-let playerSign = "X";
-let runBot = true;
+// Randomly place food
+function placeFood() {
 
-
-function clickedBox(element){
-    if(players.classList.contains("player")){
-        playerSign = "O";
-        element.innerHTML = `<i class="${playerOIcon}"></i>`;
-        players.classList.remove("active");
-        element.setAttribute("id", playerSign);
-    }else{
-        element.innerHTML = `<i class="${playerXIcon}"></i>`;
-        element.setAttribute("id", playerSign);
-        players.classList.add("active");
-    }
-    selectWinner();
-    element.style.pointerEvents = "none";
-    playBoard.style.pointerEvents = "none";
-    let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed();
-    setTimeout(()=>{
-        bot(runBot);
-    }, randomTimeDelay);
-}
-
-
-function bot(){
-    let array = [];
-    if(runBot){
-        playerSign = "O";
-        for (let i = 0; i < allBox.length; i++) {
-            if(allBox[i].childElementCount == 0){
-                array.push(i);
-            }
-        }
-        let randomBox = array[Math.floor(Math.random() * array.length)];
-        if(array.length > 0){
-            if(players.classList.contains("player")){ 
-                playerSign = "X";
-                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
-                allBox[randomBox].setAttribute("id", playerSign);
-                players.classList.add("active");
-            }else{
-                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
-                players.classList.remove("active");
-                allBox[randomBox].setAttribute("id", playerSign);
-            }
-            selectWinner();
-        }
-        allBox[randomBox].style.pointerEvents = "none";
-        playBoard.style.pointerEvents = "auto";
-        playerSign = "X";
-    }
-}
-
-function getIdVal(classname){
-    return document.querySelector(".box" + classname).id;
-}
-function checkIdSign(val1, val2, val3, sign){
-    if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
-        return true;
-    }
-}
-function selectWinner(){
-    if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9, playerSign) || checkIdSign(1,5,9, playerSign) || checkIdSign(3,5,7, playerSign)){
-        runBot = false;
-        bot(runBot);
-        setTimeout(()=>{
-            resultBox.classList.add("show");
-            playBoard.classList.remove("show");
-        }, 700);
-        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`;
-    }else{
-        if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
-            runBot = false;
-            bot(runBot);
-            setTimeout(()=>{
-                resultBox.classList.add("show");
-                playBoard.classList.remove("show");
-            }, 700);
-            wonText.textContent = "Match has been drawn!";
-        }
-    }
-}
-
-replayBtn.onclick = ()=>{
-    window.location.reload();
+	// in x coordinates.
+	foodX = Math.floor(Math.random() * total_col) * blockSize;
+	
+	//in y coordinates.
+	foodY = Math.floor(Math.random() * total_row) * blockSize;
 }
